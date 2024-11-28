6cd8b21a5f443123c87a313e5c3f4d5facad170b
Nickisew <nicole.sewell@outlook.com>
2024-11-28 11:01:18 -0500

    video update

--- whale-talk.js
+++ whale-talk.js
@@ -1,35 +1,32 @@
-var input = `He likes you`;
+var input = `HE likes you`;
 
 const vowels = ["a", "e", "i", "o", "u"];
-var resultsArray = [];
+var resultsArray = []; // change var to const
 
 for (let i = 0; i < input.length; i++) {
-  let letter = input[i];
+  let letter = input[i].toLowerCase();
 
-  // console.log(i);
+  //   console.log(i);
+
+  if (letter === "e" || letter === "u") {
+    resultsArray.push(letter);
+  }
 
   for (let j = 0; j < vowels.length; j++) {
     let vowel = vowels[j];
-    // console.log(i);
+    //  console.log(j);
 
     if (letter === vowel) {
-      resultsArray.push(vowel);
-    }
-
-    for (let k = 0; k < resultsArray.length; k++) {
-      let vowel2 = resultsArray[k];
-
-      if (vowel2 === "u") {
-        resultsArray.push("u");
-      }
-
-      if (vowel2 === "e") {
-        resultsArray.push("e");
-      }
+      //   console.log(letter);
+      resultsArray.push(letter);
     }
   }
 }
 
+console.log(resultsArray);
+
 let resultString = resultsArray.join(" ");
 
 resultString = resultString.toUpperCase();
+
+console.log(resultString);
